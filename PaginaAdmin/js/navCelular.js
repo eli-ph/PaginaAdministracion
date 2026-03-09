98870932c4520a6323a80cd2cbaad065d7de9de7
Elizabeth Portilla <elysph9@gmail.com>
2026-03-08 18:08:33 -0600

    Dejar solo carpeta principal

--- PaginaAdmin/js/navCelular.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    const toggle = document.getElementById("nav");
-  const links = document.getElementById("navLinks");
-
-  toggle.addEventListener("click", () => {
-    links.classList.toggle("active");
-  });
-})();
